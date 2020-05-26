6d4e9e0310fffd6f51ee0cd7bc079c605d14a2c1
moriokameda <mkame19941031@gmail.com>
2020-05-27 00:17:20 +0900

    footer完成

--- js/main.js
+++ js/main.js
@@ -1,43 +1,45 @@
 'use strict';
 
+var imgHeader = document.getElementsByClassName('section1_item');
+imgHeader.children;
 
-  var content1 = $('#content1');
-  var content2 = $('#content2');
-  var content3 = $('#content3');
-  var content4 = $('#content4');
-  var heroHeader = $('.ul-item');
+  // var content1 = $('#content1');
+  // var content2 = $('#content2');
+  // var content3 = $('#content3');
+  // var content4 = $('#content4');
+  // var heroHeader = $('.ul-item');
 
-  heroHeader.children('a').on('click', function () {
-    heroHeader.removeClass('hero-active');
-    var data = $(this).attr('data-target');
-    $(this).parent('li').addClass('hero-active');
-    console.log(data);
-    switch (data) {
-      case data === content1.attr('id'):
-        content1.addClass('hero-active');
-        break;
-      case data === content2.attr('id'):
-        content2.addClass('hero-active');
-        break;
-      case data === content3.attr('id'):
-        content3.addClass('hero-active');
-        break;
-      case data === content4.attr('id'):
-        content4.addClass('hero-active');
-        break;
+  // heroHeader.children('a').on('click', function () {
+  //   heroHeader.removeClass('hero-active');
+  //   var data = $(this).attr('data-target');
+  //   $(this).parent('li').addClass('hero-active');
+  //   console.log(data);
+  //   switch (data) {
+  //     case data === content1.attr('id'):
+  //       content1.addClass('hero-active');
+  //       break;
+  //     case data === content2.attr('id'):
+  //       content2.addClass('hero-active');
+  //       break;
+  //     case data === content3.attr('id'):
+  //       content3.addClass('hero-active');
+  //       break;
+  //     case data === content4.attr('id'):
+  //       content4.addClass('hero-active');
+  //       break;
 
-      default:
-        content1.addClass('hero-active');
-        break;
-    }
-    return data;
-  });
-
-  // content1.hover(function () {
-  //   if (this.hasClass('hero-active')) {
-  //     return;
-  //   } else {
-  //     heroHeader.removeClass('hero-active');
-  //     content1.addClass('hero-active');
+  //     default:
+  //       content1.addClass('hero-active');
+  //       break;
   //   }
+  //   return data;
   // });
+
+  // // content1.hover(function () {
+  // //   if (this.hasClass('hero-active')) {
+  // //     return;
+  // //   } else {
+  // //     heroHeader.removeClass('hero-active');
+  // //     content1.addClass('hero-active');
+  // //   }
+  // // });
